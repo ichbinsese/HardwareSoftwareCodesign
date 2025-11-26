33d97985b6c8fc4d46fc1639c462d2e3536c8319
ichbinsese <sesemerk@gmail.com>
2025-11-26 18:22:28 +0100

    First Try Creating UDP protocol from Vitis Example Vitis Project is located in Vitis-Project New Vivado Project is called test_2

---
+++ Vitis-Project/test/zynq_fsbl/zynq_fsbl_bsp/libsrc/build_configs/gen_bsp/CMakeFiles/clean_additional.cmake
@@ -0,0 +1,19 @@
+# Additional clean files
+cmake_minimum_required(VERSION 3.16)
+
+if("${CONFIG}" STREQUAL "" OR "${CONFIG}" STREQUAL "")
+  file(REMOVE_RECURSE
+  "C:\\Projects\\HWSW\\Vitis-Project\\test\\zynq_fsbl\\zynq_fsbl_bsp\\include\\diskio.h"
+  "C:\\Projects\\HWSW\\Vitis-Project\\test\\zynq_fsbl\\zynq_fsbl_bsp\\include\\ff.h"
+  "C:\\Projects\\HWSW\\Vitis-Project\\test\\zynq_fsbl\\zynq_fsbl_bsp\\include\\ffconf.h"
+  "C:\\Projects\\HWSW\\Vitis-Project\\test\\zynq_fsbl\\zynq_fsbl_bsp\\include\\sleep.h"
+  "C:\\Projects\\HWSW\\Vitis-Project\\test\\zynq_fsbl\\zynq_fsbl_bsp\\include\\xilffs.h"
+  "C:\\Projects\\HWSW\\Vitis-Project\\test\\zynq_fsbl\\zynq_fsbl_bsp\\include\\xilffs_config.h"
+  "C:\\Projects\\HWSW\\Vitis-Project\\test\\zynq_fsbl\\zynq_fsbl_bsp\\include\\xilrsa.h"
+  "C:\\Projects\\HWSW\\Vitis-Project\\test\\zynq_fsbl\\zynq_fsbl_bsp\\include\\xiltimer.h"
+  "C:\\Projects\\HWSW\\Vitis-Project\\test\\zynq_fsbl\\zynq_fsbl_bsp\\include\\xtimer_config.h"
+  "C:\\Projects\\HWSW\\Vitis-Project\\test\\zynq_fsbl\\zynq_fsbl_bsp\\lib\\libxilffs.a"
+  "C:\\Projects\\HWSW\\Vitis-Project\\test\\zynq_fsbl\\zynq_fsbl_bsp\\lib\\libxilrsa.a"
+  "C:\\Projects\\HWSW\\Vitis-Project\\test\\zynq_fsbl\\zynq_fsbl_bsp\\lib\\libxiltimer.a"
+  )
+endif()

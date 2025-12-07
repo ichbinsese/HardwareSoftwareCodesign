deca5320d7e66f6a0aa2e6729b8199579ccbd994
Abdulrahman Demirkiran <abdu.demirkiran007@gmail.com>
2025-12-07 09:19:46 +0100

    PY

--- Vitis_Project/_ide/workspace_journal.py
+++ Vitis_Project/_ide/workspace_journal.py
@@ -7,3 +7,5 @@ client.set_workspace(path="Vitis_Project")
 platform = client.get_component(name="soc_platform")
 status = platform.update_hw(hw_design = "$COMPONENT_LOCATION/../../Vivado_Project/SoC_Project/soc_design_wrapper.xsa")
 
+vitis.dispose()
+
